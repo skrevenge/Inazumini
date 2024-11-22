d5d530b6023445fe6b17fd60dac1c6ef442c0ac8
skrevenge <57811892+skrevenge@users.noreply.github.com>
2024-11-22 19:17:43 -0300

    Update PlayerStatsV3.js

--- PlayerStatsV3.js
+++ PlayerStatsV3.js
@@ -124,7 +124,147 @@ class PlayerStats {
                 CpuRankURHst: ['Shippuu Dash', 'Dark Phoenix', 'Bunshin Defense'],
                 CpuRankLHst: ['Shippuu Dash', 'Dark Phoenix', 'Bunshin Defense', 'Fuujin no Mai']
             }),
-            // Adicione mais personagens conforme necessário.
+            'mark01': new PlayerStats.Character({
+                name: 'Mark Evans',
+                undubName: 'Mamoru Endou',
+                TP: 180,
+                FP: 170,
+                attribute: 'Earth',
+                shoot: 50,
+                dribble: 60,
+                speed: 70,
+                strength: 75,
+                keeper: 90,
+                hissatsu: ['God Hand', 'Burning Punch'],
+                headSpriteConfig: {
+                    key: 'raimonHead',
+                    frames: [0, 1, 2, 3, 4, 5, 6, 7]
+                },
+                portraitFrame: 0,
+                LvUpBoost: {
+                    1: [5, 4, 1, 2, 2, 2, 3],
+                    2: [6, 5, 2, 2, 2, 2, 3],
+                    3: [6, 5, 2, 3, 3, 3, 4],
+                    4: [7, 6, 2, 3, 3, 3, 4],
+                    5: [7, 6, 3, 3, 3, 3, 5]
+                },
+                RankUpBoost: {
+                    RankRare: [10, 8, 3, 4, 4, 4, 6],
+                    RankSRare: [15, 12, 5, 6, 6, 6, 9],
+                    RankURare: [20, 16, 7, 8, 8, 8, 12],
+                    RankLegend: [25, 20, 9, 10, 10, 10, 15]
+                },
+                CpuRankRHst: ['God Hand'],
+                CpuRankSRHst: ['God Hand', 'Majin The Hand'],
+                CpuRankURHst: ['God Hand', 'Majin The Hand', 'Fist of Justice'],
+                CpuRankLHst: ['God Hand', 'Majin The Hand', 'Fist of Justice', 'Hammer of Wrath']
+            }),
+           'mark02': new PlayerStats.Character({
+                name: 'Mark Evans',
+                undubName: 'Mamoru Endou',
+                TP: 180,
+                FP: 170,
+                attribute: 'Earth',
+                shoot: 50,
+                dribble: 60,
+                speed: 70,
+                strength: 75,
+                keeper: 90,
+                hissatsu: ['God Hand', 'Burning Punch'],
+                headSpriteConfig: {
+                    key: 'raimonHead',
+                    frames: [0, 1, 2, 3, 4, 5, 6, 7]
+                },
+                portraitFrame: 0,
+                LvUpBoost: {
+                    1: [5, 4, 1, 2, 2, 2, 3],
+                    2: [6, 5, 2, 2, 2, 2, 3],
+                    3: [6, 5, 2, 3, 3, 3, 4],
+                    4: [7, 6, 2, 3, 3, 3, 4],
+                    5: [7, 6, 3, 3, 3, 3, 5]
+                },
+                RankUpBoost: {
+                    RankRare: [10, 8, 3, 4, 4, 4, 6],
+                    RankSRare: [15, 12, 5, 6, 6, 6, 9],
+                    RankURare: [20, 16, 7, 8, 8, 8, 12],
+                    RankLegend: [25, 20, 9, 10, 10, 10, 15]
+                },
+                CpuRankRHst: ['God Hand'],
+                CpuRankSRHst: ['God Hand', 'Majin The Hand'],
+                CpuRankURHst: ['God Hand', 'Majin The Hand', 'Fist of Justice'],
+                CpuRankLHst: ['God Hand', 'Majin The Hand', 'Fist of Justice', 'Hammer of Wrath']
+            }),
+          'nathan01': new PlayerStats.Character({
+                name: 'Nathan Swift',
+                undubName: 'Kazemaru Ichirouta',
+                TP: 160,
+                FP: 180,
+                attribute: 'Wind',
+                shoot: 60,
+                dribble: 80,
+                speed: 90,
+                strength: 65,
+                keeper: 40,
+                hissatsu: ['Shippuu Dash', 'Dark Phoenix'],
+                headSpriteConfig: {
+                    key: 'raimonHead',
+                    frames: [8, 9, 10, 11, 12, 13, 14, 15]
+                },
+                portraitFrame: 1,
+                LvUpBoost: {
+                    1: [4, 5, 2, 3, 3, 2, 1],
+                    2: [5, 6, 2, 3, 3, 2, 1],
+                    3: [5, 6, 3, 4, 4, 3, 1],
+                    4: [6, 7, 3, 4, 4, 3, 1],
+                    5: [6, 7, 3, 4, 5, 3, 2]
+                },
+                RankUpBoost: {
+                    RankRare: [8, 10, 4, 5, 6, 4, 3],
+                    RankSRare: [12, 15, 6, 8, 9, 6, 4],
+                    RankURare: [16, 20, 8, 10, 12, 8, 6],
+                    RankLegend: [20, 25, 10, 12, 15, 10, 8]
+                },
+                CpuRankRHst: ['Shippuu Dash'],
+                CpuRankSRHst: ['Shippuu Dash', 'Dark Phoenix'],
+                CpuRankURHst: ['Shippuu Dash', 'Dark Phoenix', 'Bunshin Defense'],
+                CpuRankLHst: ['Shippuu Dash', 'Dark Phoenix', 'Bunshin Defense', 'Fuujin no Mai']
+            }),
+          'nathan02': new PlayerStats.Character({
+                name: 'Nathan Swift',
+                undubName: 'Kazemaru Ichirouta',
+                TP: 160,
+                FP: 180,
+                attribute: 'Wind',
+                shoot: 60,
+                dribble: 80,
+                speed: 90,
+                strength: 65,
+                keeper: 40,
+                hissatsu: ['Shippuu Dash', 'Dark Phoenix'],
+                headSpriteConfig: {
+                    key: 'raimonHead',
+                    frames: [8, 9, 10, 11, 12, 13, 14, 15]
+                },
+                portraitFrame: 1,
+                LvUpBoost: {
+                    1: [4, 5, 2, 3, 3, 2, 1],
+                    2: [5, 6, 2, 3, 3, 2, 1],
+                    3: [5, 6, 3, 4, 4, 3, 1],
+                    4: [6, 7, 3, 4, 4, 3, 1],
+                    5: [6, 7, 3, 4, 5, 3, 2]
+                },
+                RankUpBoost: {
+                    RankRare: [8, 10, 4, 5, 6, 4, 3],
+                    RankSRare: [12, 15, 6, 8, 9, 6, 4],
+                    RankURare: [16, 20, 8, 10, 12, 8, 6],
+                    RankLegend: [20, 25, 10, 12, 15, 10, 8]
+                },
+                CpuRankRHst: ['Shippuu Dash'],
+                CpuRankSRHst: ['Shippuu Dash', 'Dark Phoenix'],
+                CpuRankURHst: ['Shippuu Dash', 'Dark Phoenix', 'Bunshin Defense'],
+                CpuRankLHst: ['Shippuu Dash', 'Dark Phoenix', 'Bunshin Defense', 'Fuujin no Mai']
+            }),
+            // Add more players here as needed
         };
     }
 
