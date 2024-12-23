77027e6639b69ce5b7e5c0bff59bf10de1f5ca44
skrevenge <57811892+skrevenge@users.noreply.github.com>
2024-12-23 17:18:51 -0300

    Update HissatsuDatabase.js

--- HissatsuDatabase.js
+++ HissatsuDatabase.js
@@ -24,6 +24,54 @@ class HissatsuDatabase {
                 starRank: 1,
                 hPortrait: 1,
                 animation: null // Will be implemented later
+            },
+            'FireTornado': {
+                name: "Fire Tornado",
+                undub_name: "Fire Tornado",
+                loc_name: "Furacão de Fogo", 
+                tp_cost: 35,
+                attribute: "Fire",
+                type: "Shoot",
+                power: 70,
+                starRank: 1,
+                hPortrait: 1,
+                animation: null // Will be implemented later
+            },
+            'TheWall': {
+                name: "The Wall",
+                undub_name: "The Wall",
+                loc_name: "O Muro", 
+                tp_cost: 30,
+                attribute: "Earth",
+                type: "Defense",
+                power: 50,
+                starRank: 1,
+                hPortrait: 1,
+                animation: null // Will be implemented later
+            },
+            'ShippuuDash': {
+                name: "Wind Dash",
+                undub_name: "Shipuu Dash",
+                loc_name: "Deslize Veloz", 
+                tp_cost: 30,
+                attribute: "Wind",
+                type: "Dribble",
+                power: 60,
+                starRank: 1,
+                hPortrait: 1,
+                animation: null // Will be implemented later
+            },
+            'TamanoriPierrot': {
+                name: "Rodeo Clown",
+                undub_name: "Tamanori Pierrot",
+                loc_name: "Palhaço de Rodeio", 
+                tp_cost: 20,
+                attribute: "Wind",
+                type: "Dribble",
+                power: 40,
+                starRank: 1,
+                hPortrait: 1,
+                animation: null // Will be implemented later
             }
         };
     }
